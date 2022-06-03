d64822d6d361a391b5255a3209dce33288c32f3f
Urus4 <102502766+Urus4@users.noreply.github.com>
2022-06-03 19:16:59 +0900

    C++ study

---
+++ C++/ch6_openchallenge/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+class RandomNum {
+private:
+	int Num;
+public:
+	void getRandomNum();
+	int getNum();
+	
+};
+
+void RandomNum::getRandomNum() {
+	srand((unsigned)time(0));
+	int n = rand();
+	Num = n % 100;
+}
+
+int RandomNum::getNum(){
+	return Num;
+}
+
+//======================================================
+class Player {
+private:
+	string name;
+public:
+	void setName(string name);
+	string getName();
+
+};
+
+void Player::setName(string name) {
+	this->name = name;
+}
+
+string Player::getName() {
+	return name;
+}
+
+//=====================================================
+class Compare{
+private:
+	int Inputvalue;
+	int Rvalue;
+	int Lvalue;
+public:
+	Compare();
+	int comparevalue(int Num);
+	void noticeRange();
+	void setInputvalue(int value);
+
+};
+
+Compare::Compare() {
+	Inputvalue = 0;
+	Rvalue = 99;
+	Lvalue = 0;
+}
+
+int Compare::comparevalue(int Num){
+
+	int mid;
+	
+	mid = (Rvalue + Lvalue) / 2;
+	
+
+	if (Num == Inputvalue)
+	{
+		cout << "김인수가 이겼습니다!!\n";
+		return 1;
+	}
+	else if ((Num != Inputvalue) && (Num < Inputvalue))
+	{
+		Rvalue = Inputvalue;
+		return 0;
+	}
+	else if ((Num != Inputvalue) && (Num > Inputvalue))
+	{
+		Lvalue = Inputvalue;
+		return 0;
+	}
+}
+
+void Compare::noticeRange() {
+
+	cout << "답은 " << Lvalue << "과 " << Rvalue << " 사이에 있습니다.\n";
+}
+
+void Compare::setInputvalue(int value) {
+	Inputvalue = value;
+}
+
+
+
+//================================================================================================================================================
+int main() {
+
+	RandomNum random;
+	Player player[5];
+	Compare compare;
+	int num;
+	int input;
+	int playernum;
+	int j=0;
+
+
+	random.getRandomNum();
+	num = random.getNum();
+
+
+	cout << "게임에 참가할 인원수는 몇명입니까?(최대 5명) : ";
+	cin >> playernum;
+
+	for (int i = 0; i < playernum; i++)
+	{
+		string name;
+		cout << "참가자의 이름을 적어주십시오 : ";
+		cin >> name;
+		player[i].setName(name);
+
+	}
+
+	cout << "Up & Down 게임을 시작합니다.\n";
+
+	while (1)
+	{
+		compare.noticeRange();
+		cout << player[j++].getName() << ">>";
+		cin >> input;
+		compare.setInputvalue(input);
+		if (compare.comparevalue(num))
+		{
+			break;
+		}
+		if (j == playernum) {
+			j = 0;
+		}
+	}
+
+	return 0;
+}
